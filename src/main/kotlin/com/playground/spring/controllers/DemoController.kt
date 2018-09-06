d3a0fbf277a946ec445936d74d7f5498f23c210e
Henry <th.saravana@gmail.com>
2018-09-06 13:55:39 +0530

    Handlebars via Templates

--- src/main/kotlin/com/playground/spring/controllers/DemoController.kt
+++ src/main/kotlin/com/playground/spring/controllers/DemoController.kt
@@ -1,12 +1,13 @@
 package com.playground.spring.controllers
 
 import com.github.jknack.handlebars.Handlebars
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class DemoController {
 
     @RequestMapping(value = ["/hello"], produces = ["application/json"])
@@ -15,13 +16,25 @@ class DemoController {
         return ResponseEntity(Response("Hello World"), HttpStatus.OK)
     }
 
-    @RequestMapping(value = ["/handlebars"], produces = ["application/json"])
-    fun handlebars(): ResponseEntity<Response> {
+    @RequestMapping(value = ["/handlebars/inline"], produces = ["application/json"])
+    fun inlineHandlebars(): ResponseEntity<Response> {
         val handlebars = Handlebars()
         val template = handlebars.compileInline("Parsing {{this}} value")
         val parsedValue = template.apply("sample")
         return ResponseEntity(Response(parsedValue), HttpStatus.OK)
     }
+
+    @RequestMapping(value = ["/handlebars"], produces = ["application/json"])
+    fun handlebars(): ResponseEntity<Response> {
+        val loader = ClassPathTemplateLoader()
+        loader.prefix = "/templates"
+        val handlebars = Handlebars(loader)
+        val template = handlebars.compile("expressions")
+        val parsedValue = template.apply(Sample("a", "b"))
+        return ResponseEntity(Response(parsedValue), HttpStatus.OK)
+    }
 }
 
 data class Response(val key: String)
+
+data class Sample(val a: String, val b: String)
